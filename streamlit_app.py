0f1b39b7450db542469b83c86a72ed7bf5382635
Quinn Daly <quinndaly05@gmail.com>
2026-01-29 19:36:13 +0000

    commit#

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from openai import OpenAI
 
-lab01 = st.Page('./lab01.py', title='Lab 01: Build a Streamlit Document Q&A app')
-lab02 = st.Page('./lab02.py', title='Lab 02: Document Summarizer')
+lab1 = st.Page('lab1.py', title='Lab 1: Build a Streamlit Document Q&A app')
+lab2 = st.Page('lab2.py', title='Lab 2: Document Summarizer')
 
-pg = st.navigation(pages = [lab01, lab02], position="sidebar", expanded=False)
+pg = st.navigation(pages = [lab1, lab2], position="sidebar", expanded=False)
 st.set_page_config(page_title="IST488 Labs", page_icon=None, layout="wide", initial_sidebar_state="expanded", menu_items=None)
 pg.run()
